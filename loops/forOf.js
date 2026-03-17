1b7dfc4a63c313410ce923e2db0db60d8eb3c5da
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-03-17 23:28:10 +0600

    loop part in forOf Loop done

---
+++ loops/forOf.js
@@ -0,0 +1,23 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for (const num of numbers) {
+    // console.log(num);
+}
+
+//---------
+
+let subjects = ['bangla', 'english', 'math', 'arabic']
+for (const item of subjects) {
+    // console.log(item);
+}
+
+// --------------
+let bornYear = [2004, 1978, 1976, 1998, 1993]
+for (const item of bornYear) {
+    // console.log(item);
+}
+
+// ---------
+let examDates = ['12.3.25', '14.3.25', '17.3.25', '19.3.25']
+for (const date of examDates) {
+    console.log(date)
+}

08428dd70b1a7f7e5aad3381e3521580b6a95a36
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-03-26 18:25:13 +0600

    more code added

--- object/ps4.js
+++ object/ps4.js
@@ -43,3 +43,67 @@ for (const key in phone) {
 }
 
 //! practice 
+const books = {
+    s1: 'bangla',
+    s2: 'english',
+    s3: 'math',
+    s4: 'arabic'
+}
+let bookKeys = Object.keys(books)
+// console.log(bookKeys);
+
+let bookValues = Object.values(books)
+// console.log(bookValues);
+
+// ---------
+
+const author = {
+    title: 'learn Js',
+    category: 'programming'
+}
+let authorExist = Object.keys(author)
+let exist = authorExist.includes('author')
+// console.log(exist);
+
+// -------
+
+const laptops = {
+    name: 'dell',
+    brand: 'Dell',
+    model: 'inspiron',
+    price: 40000
+}
+for (const key in laptops) {
+    const value = laptops[key]
+    // console.log(key, ':', value);
+}
+
+// -----
+
+const phones = {
+    brand: 'samsung',
+    model: 'galaxy s21',
+    price: 85000
+}
+let keys = Object.keys(phones)
+for (const key of keys) {
+    // console.log(key, phones[key]);
+}
+
+// ----
+
+const bikes = {
+    brand: 'hero',
+    price: 120000,
+    model: 'splendor'
+}
+let vlues = Object.values(bikes)
+// console.log(vlues);
+
+// -------
+
+const books1 = {
+    b1: 'hp',
+    b2: 'th',
+    b3: 'Got'
+}

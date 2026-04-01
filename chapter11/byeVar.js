d6ef9e3ef77f5ad57d2edc715c64882e4d365061
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-01 17:54:42 +0600

    chapter11 strat to learn es6 topics

---
+++ chapter11/byeVar.js
@@ -0,0 +1,26 @@
+//! cannot change declared variable values
+const money = 35;
+// money = 50; //not allowed
+// console.log(money);
+
+// ---------
+const moneys = 35
+const rich = money + 50;
+// console.log(rich);
+
+
+//*can changed when used array ---------
+const num = [1, 2, 3,]
+// console.log(num);
+num[3]=4
+// console.log(num);
+num[1]=4
+// console.log(num);
+
+
+//! can change declared variable values
+let nam = 'miraj'
+// console.log(nam);
+nam = 'mirajs'
+// console.log(nam);
+

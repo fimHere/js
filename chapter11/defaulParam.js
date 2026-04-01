16969ba49f3f8cf599b44574d6248cf75ca6de51
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-01 21:06:45 +0600

    some more code added

--- chapter11/defaulParam.js
+++ chapter11/defaulParam.js
@@ -30,10 +30,40 @@ function createUser(name = '', age = 0, education = {}, hobbies = [], gonVag = 1
         gonVag,
         jogBiyog
     }
-    console.log(user)
+    // console.log(user)
 }
 createUser()
 createUser("miraj", 25, { class: 'seven', isClass: true }, ['tour', 'exploring'])
 
 
 //! practice
+function add(a = 0, b = 0, c = 0) {
+    let result = a + b + c
+
+    return result
+}
+let output = add(5, 5, 5)
+// console.log(output);
+
+// --------
+function deposit(a = 50) {
+    let pay = a
+
+    return pay
+}
+let availableBalance = deposit(100)
+// console.log(`user pay ${availableBalance}`);
+let availableBalances = deposit()
+// console.log(`default value ${availableBalances}`);
+
+// -------
+function userProfile(name = 'anonymous', income = 0) {
+
+    return [name, income]
+}
+let userItem = userProfile()
+// console.log(`default value ${userItem}`);
+let userItem1 = userProfile('miraj', '10k')
+// console.log(userItem1);
+
+// -------

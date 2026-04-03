0426402160e734df3691a2d54a9cd48478856213
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-03 20:17:28 +0600

    arrow function vs regular function define with this by justify

--- chapter11/arrowFunc.js
+++ chapter11/arrowFunc.js
@@ -4,17 +4,41 @@ function add(a, b) {
     return sum
 }
 let result = add(5, 10)
-console.log(result);
+// console.log(result);
 
 //! to sort way to define a function
 function add(a, b) {
     return a + b
 }
 let results = add(15, 10)
-console.log(results);
+// console.log(results);
 
 //! function expression
 const sum = function (a, b) {
     return a + b
 }
-console.log(sum(1, 2));
+// console.log(sum(1, 2));
+
+//* arrow function
+const sums = (a, b) => {
+    return a + b
+}
+// console.log(sums(3, 2));
+
+//* another way to define arrow function
+const sums1 = (a, b) => a + b
+// console.log(sums(3, 3));
+
+//! regular func VS arrow func
+const mySelf = {
+    name: 'miraj',
+    age: 25,
+    sayRegularFun: function () {
+        console.log(`Hello regular, this is ${this.name}, age is ${this.age}`);
+    },
+    sayArrowFunc: () => {
+        console.log(`Hello arrow, this is ${this.name}, age is ${this.age}`);
+    }
+}
+mySelf.sayRegularFun()
+mySelf.sayArrowFunc()

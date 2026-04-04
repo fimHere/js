8ca93fc2466111c66ed664d131c5f6aa9a075df6
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-04 20:00:46 +0600

    destructuring practice part done

--- chapter12/destructuring.js
+++ chapter12/destructuring.js
@@ -44,10 +44,68 @@ const arrDest = ([name, age]) => {
     return `Name is ${name}, Age is ${age}`
 }
 const arrDestResult = arrDest(['siraj', 25])
-console.log(arrDestResult);
+// console.log(arrDestResult);
 
 //* skipping value in array destructuring
 const numbers = [1, 2, 3, 4]
 const [first, , , forth] = numbers //! skip tow and three number values
 // console.log(first, forth);
 
+
+//! practice
+//*1
+const product = {
+    name: 'laptop',
+    price: 5000,
+    brand: 'dell'
+}
+const { brand } = product
+// console.log(brand);
+
+//*2
+const item = {
+    name: 'mobile',
+    price: 20000,
+    phon: 'samsung'
+}
+const { price, phon } = item
+// console.log(price, phon);
+
+//*3
+const colors = ['red', 'green', 'blue']
+const [firsts, second] = colors
+// console.log(firsts, second);
+
+//*4
+const arr3num = [1, 2, 3]
+const [, seconds] = arr3num
+// console.log(seconds);
+
+//*5
+const digits = [2, 4, 6, 8]
+const [tow, , , eight] = digits
+// console.log(tow, eight);
+
+//*6
+function arrayDest(a, b) {
+    return [a * 3, b * 3]
+}
+let result = arrayDest(3, 4)
+// console.log(result);
+
+//*7
+const nPerson = {
+    names: 'rahim',
+    city: 'dhaka'
+}
+const { names, city, phn = "N/A" } = nPerson
+// console.log(names, city, phn);
+
+//*8
+const teacher = {
+    name: 'maria',
+    profession: 'teacher'
+}
+// console.log(teacher);
+const { nam, profession: alias } = teacher
+// console.log(alias);

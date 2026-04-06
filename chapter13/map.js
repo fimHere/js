660bb00fd53741e1e3633b4aabb1c101c49aee7a
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-06 14:45:35 +0600

    new objects learn to map

--- chapter13/map.js
+++ chapter13/map.js
@@ -1,13 +1,20 @@
+//! full explanation double number return
 const numbers = [1, 2, 3, 4, 5, 6]
 function numDouble(numbers) {
     const double = []
-    
+
     for (const num of numbers) {
         const value = num * 2
-        
+
         double.push(value)
     }
-    
+
     return double
 }
 // console.log(numDouble(numbers));
+
+//! double number return by map methods
+const numbers0 = [2, 4, 6, 8]
+const result = numbers0.map(num => num * 2)
+console.log(result);
+

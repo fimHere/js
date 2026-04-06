d8f214dbfdab53e9a67ab32da0d2a7e5faf23bb7
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-06 18:26:17 +0600

    map new objects learned

--- chapter13/map.js
+++ chapter13/map.js
@@ -13,8 +13,23 @@ function numDouble(numbers) {
 }
 // console.log(numDouble(numbers));
 
-//! double number return by map methods
+//! (gun, jug, vag) number return by map methods
 const numbers0 = [2, 4, 6, 8]
-const result = numbers0.map(num => num * 2)
-console.log(result);
+const result = numbers0.map(num => num * 2) //*gun
+const result0 = numbers0.map(num => num + 5) //*jug
+const result1 = numbers0.map(num => num / 2) //*vag 
+// console.log(result);
+// console.log(result0);
+// console.log(result1);
 
+//! array element length return by map
+const names = ['miraj', 'siraj', 'labib', 'shahajadi', 'rayhan']
+const nameResult = names.map(name => name.length)
+// console.log(nameResult);
+
+//! array element values first and last latter return by map, at methods
+const names0 = ['miraj', 'siraj', 'labib', 'shahajadi', 'rayhan']
+const nameResult0 = names.map(name => name[0]) //*return arrays values first latter
+const nameResult00 = names.map(name => name.at(-1)) //*return arrays values last latter
+// console.log(nameResult0);
+// console.log(nameResult00);

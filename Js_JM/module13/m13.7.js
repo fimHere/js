8dd37c6c3062e16cc126d649873a0fbc58a5cde9
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-12 11:12:38 +0600

    module13.7 is done

---
+++ Js_JM/module13/m13.7.js
@@ -0,0 +1,12 @@
+/**
+ *^ naming convention of variable
+ ** 1. do not use special keyword in variable name
+ ** 2. do not use white space or gap in variable name
+ ** 3. do not use quotation in variable name
+ ** 4. can not start a number in variable name, but latter then number allowed.
+ ** 5. variable name is case sensitive
+        snake case (my_name)
+        camel case (myName)
+        pascal case (MyName)
+ */
+

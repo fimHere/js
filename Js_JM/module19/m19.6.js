2f19b40c401199ca3d516c8b07b23009a8ec402d
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-18 22:51:49 +0600

    m19.6 is done & rename file

--- Js_JM/module19/m19.6.js
+++ Js_JM/module19/m19.6.js
@@ -1,14 +1,44 @@
 /**
  *^ topic of the function
- ** 1. return
+ ** 1. conditional return
  */
 
 //^ 
-function tenTimes(number) { //* parameters
-    const result = number * 10
-    return result
+function isEven(number) {
+    if (number % 2 === 0) {
+        return `the number is even: ${true}`
+    } else {
+        return `the number is even: ${false}`
+    }
 }
+console.log(isEven(5));
+console.log(isEven(4));
 
-//^
-const output = tenTimes(10) //* arguments
-console.log(output);
+function isOdd(number) {
+    if (number % 2 !== 0) {
+        return `the number is odd: ${true}`
+    } else {
+        return `the number is odd: ${false}`
+    }
+}
+console.log(isOdd(5));
+console.log(isOdd(4));
+// or
+function isEven(number) {
+    if (number % 2 === 0) {
+        return `the number is even: ${true}`
+    }
+    return `the number is even: ${false}`
+
+}
+console.log(isEven(5));
+console.log(isEven(4));
+
+function isOdd(number) {
+    if (number % 2 !== 0) {
+        return `the number is odd: ${true}`
+    }
+    return `the number is odd: ${false}`
+}
+console.log(isOdd(5));
+console.log(isOdd(4));

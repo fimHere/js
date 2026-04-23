a63a627b5298700351a92d3b0930437a2302d743
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-23 11:55:32 +0600

    m24  vedio 2 is done

--- Js_JM/module24/main.js
+++ Js_JM/module24/main.js
@@ -1,4 +1,30 @@
-console.log('hello im outside of html');
+const heading = document.getElementsByTagName('h1')
+console.log('heading:', heading);
 
-console.log(document.head);
-console.log(document.body);
+const paragraph = document.getElementsByTagName('p')
+console.log('paragraph:', paragraph);
+
+const ulItem = document.getElementsByTagName('ul')
+console.log('ulItem:', ulItem);
+
+const listItem = document.getElementsByTagName('li')
+console.log('listItem:', listItem);
+
+const anchorItem = document.getElementsByTagName('a')
+console.log("anchorItem:", anchorItem);
+
+const navItem = document.getElementsByTagName('nav')
+console.log("navItem:", navItem);
+
+function itemsLoop(params) {
+    for (const element of params) {
+        console.log(element);
+        console.log(element.textContent);
+    }
+}
+itemsLoop(heading)
+itemsLoop(paragraph)
+itemsLoop(ulItem)
+itemsLoop(listItem)
+itemsLoop(anchorItem)
+itemsLoop(navItem)

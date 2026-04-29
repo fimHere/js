40d919b875d9619d480d8fe8d08a6a677ab3fb22
itsFakhrulIslam <fakhrulishere@gmail.com>
2026-04-29 16:07:54 +0600

    login button functionalities done.

---
+++ Js_JM/module26/payooApp/app.js
@@ -0,0 +1,9 @@
+// console.log('app file connected...');
+
+// login button functionalities.
+const loginBtn = document.getElementById('loginBtn')
+// console.log(loginBtn);
+loginBtn.addEventListener('click', function (event) {
+    console.log('login button clicked.');
+    event.preventDefault()
+})

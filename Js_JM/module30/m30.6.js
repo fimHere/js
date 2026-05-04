37e1094e2b39f35c1dca4625ae0293a37534df19
fimsHere <gmtelecom9089@gmail.com>
2026-05-04 07:50:47 +0600

    m30.6 is done

---
+++ Js_JM/module30/m30.6.js
@@ -0,0 +1,13 @@
+//^ topic of the class
+
+class Player {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const player0 = new Player("miraj",23);
+const player1 = new Player("siraj",32);
+console.log(player0);
+console.log(player1);
